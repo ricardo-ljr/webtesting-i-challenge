df273ee25ea49aed44717484d9d9bde5a93011ba
Ricardo Leite <ricardo.leitejr95@gmail.com>
2020-03-25 17:06:02 -0600

    MVP + Created get() method

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,21 +2,69 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    const successfulItem = {
+      name: item.name,
+      durability: item.durability,
+      enhancement: item.enhancement + 1
+    };
+    return successfulItem;
+  } else {
+    return item;
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    const failedItem = {
+      name: item.name,
+      durability: item.durability - 5,
+      enhancement: item.enhancement
+    };
+    return failedItem;
+  } else if (item.enhancement === 15 || item.enhancement === 16) {
+    const enhanceItemFail = {
+      name: item.name,
+      durability: item.durability - 10,
+      enhancement: item.enhancement
+    };
+    return enhanceItemFail;
+  } else if (item.enhancement > 16) {
+    const greatEnhanceItemFail = {
+      name: item.name,
+      durability: item.durability - 10,
+      enhancement: item.enhancement - 1
+    };
+    return greatEnhanceItemFail;
+  }
+  return item;
 }
 
 function repair(item) {
-  return { ...item };
+  const repairedItem = {
+    name: item.name,
+    durability: 100,
+    enhancement: item.enhancement
+  };
+  return repairedItem;
 }
 
 function get(item) {
-  return { ...item };
+  // TESTING STRETCH BEFORE DEPLOYING
+  // HOW DO I DO THIS T.T
+  // I THINK I GOT IT HOLY CRAP
+  if (item.enhancement === 0) {
+    return item;
+  } else {
+    const statItem = {
+      name: `[+${item.enhancement}] ${item.name}`,
+      durability: item.durability,
+      enhancement: item.enhancement
+    };
+    return statItem;
+  }
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,114 @@
-const enhancer = require('./enhancer.js');
+// MINECRAFT ITEMS
+
+// DECONSTRUC ENHANCER SO IT"S EASIER
+const { repair, succeed, fail, get } = require("./enhancer.js");
 // test away!
+// RESET REPAIR
+describe("enhancer.js", () => {
+  describe("repair()", () => {
+    it("Resets an item durability to 100", () => {
+      const item = {
+        name: "Shield",
+        durability: 35,
+        enhancement: 10
+      };
+      const fullyRepaired = {
+        name: "Shield",
+        durability: 100,
+        enhancement: 10
+      };
+      const repairedItem = repair(item);
+      expect(repairedItem).toEqual(fullyRepaired);
+    });
+  });
+  describe("fail()", () => {
+    it("Decreases durability by 5 if enhancement is less than 15", () => {
+      const item = {
+        name: "Pickaxe",
+        durability: 50,
+        enhancement: 14
+      };
+      const expected = {
+        name: "Pickaxe",
+        durability: 45,
+        enhancement: 14
+      };
+      const failedItem = fail(item);
+      expect(failedItem).toEqual(expected);
+    });
+    it("Decreases durability by 10 if enhancement equals to 15 or more, 16", () => {
+      const item = {
+        name: "Axe",
+        durability: 50,
+        enhancement: 16
+      };
+      const expected = {
+        name: "Axe",
+        durability: 40,
+        enhancement: 16
+      };
+      const failedItem = fail(item);
+      expect(failedItem).toEqual(expected);
+    });
+    it("Decreases durability by 10 and enhancement by 1 if enhancement > 16", () => {
+      const item = {
+        name: "Shovel",
+        durability: 50,
+        enhancement: 19
+      };
+      const expected = {
+        name: "Shovel",
+        durability: 40,
+        enhancement: 18
+      };
+      const failedItem = fail(item);
+      expect(failedItem).toEqual(expected);
+    });
+  });
+  describe("succeed()", () => {
+    it("raises enhancement by 1", () => {
+      const item = {
+        name: "Sword",
+        durability: 60,
+        enhancement: 17
+      };
+      const expected = {
+        name: "Sword",
+        durability: 60,
+        enhancement: 18
+      };
+      const successItem = succeed(item);
+      expect(successItem).toEqual(expected);
+    });
+    it("Enhancement level does not change if level 20", () => {
+      const item = {
+        name: "Sword",
+        durability: 60,
+        enhancement: 20
+      };
+      const expected = {
+        name: "Sword",
+        durability: 60,
+        enhancement: 20
+      };
+      const successItem = succeed(item);
+      expect(successItem).toEqual(expected);
+    });
+    describe("get()", () => {
+      it("adds enhancement level to item", () => {
+        const item = {
+          name: "Sword",
+          durability: 70,
+          enhancement: 7
+        };
+        const expected = {
+          name: "[+7] Sword",
+          durability: 70,
+          enhancement: 7
+        };
+        const getItem = get(item);
+        expect(getItem).toEqual(expected);
+      });
+    });
+  });
+});
